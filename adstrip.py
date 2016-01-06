923a7c567008d5797d198fb53c3ba1cb9dc43b69
Nick Vella <nick@nxk.me>
2016-01-07 00:27:14 +1100

    force audio sync

--- adstrip.py
+++ adstrip.py
@@ -92,7 +92,7 @@ for out_file_no in range(len(out_files)):
             filelist.write("file %s.mkv\n" % i)
 
     print('Compiling segments...')
-    os.system("ffmpeg -f concat -i %s/filelist.txt -c:v h264 -crf 18 -c:a aac -strict -2 \"%s\"" % (tmpdir.name, out_file))
+    os.system("ffmpeg -f concat -i %s/filelist.txt -c:v h264 -crf 18 -c:a aac -strict -2 -async 1 \"%s\"" % (tmpdir.name, out_file))
 
 print('Cleaning up...')
 tmpdir.cleanup()
